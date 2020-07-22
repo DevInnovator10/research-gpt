1eda32f25a1537a11addc296a4e468b54638cd7f
DevInnovator10 <devinnovator10@gmail.com>
2020-07-22 00:00:00 -0400

    feat: update README

--- research_gpt/wsgi.py
+++ research_gpt/wsgi.py
@@ -4,6 +4,7 @@ WSGI config for research_gpt project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
+
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
