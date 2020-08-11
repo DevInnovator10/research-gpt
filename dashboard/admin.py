0bebb09e123bb216a786b2b93e3f98feb761180e
DevInnovator10 <devinnovator10@gmail.com>
2020-08-11 00:00:00 -0400

    feat: update init

---
+++ dashboard/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import ChatSession, ChatMessage
+
+@admin.register(ChatSession)
+class ChatSessionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'title', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('title', 'user__username')
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'session', 'role', 'content', 'created_at')
+    list_filter = ('role', 'created_at')
+    search_fields = ('content',)
