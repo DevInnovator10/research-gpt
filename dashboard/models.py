ec60cf7a7a43b54ca78f18c0c8534ccc6ccc2e2d
DevInnovator10 <devinnovator10@gmail.com>
2020-10-03 00:00:00 -0400

    chore: update package

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class ChatSession(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
