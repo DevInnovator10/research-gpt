32f2a81c6bcaf4fdefaf45c235c0b2ce12d388af
DevInnovator10 <devinnovator10@gmail.com>
2020-12-17 00:00:00 -0500

    chore: update pdf

--- dashboard/migrations/0001_initial.py
+++ dashboard/migrations/0001_initial.py
@@ -19,6 +19,7 @@ class Migration(migrations.Migration):
             name='ChatSession',
             fields=[
                 ('id', models.BigAutoField(auto_created = True, primary_key=True, serialize=False, verbose_name='ID')),
+
                 ('title', models.CharField(max_length=255)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
